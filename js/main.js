f07cb0651f40842572ce8bec0698560d973000e5
Anish Kshatriya <anishkshatriya@Anishs-Mac-mini.home>
2015-08-31 17:51:48 -0400

    completed math for subtotal

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
   // price1 var is equal to the text of whatever is in the .price1 class
   var price1 = $(".price1").text();
-
+  var total1 = (quantity1 * price1).toFixed(2);
   // quantity1 var is equal to the text of whatever is in the .price1 class
   var quantity1 = parseInt($("input.quantity1").val());
 
@@ -17,13 +17,21 @@ $(document).ready(function() {
   $(".total1").append(price1);
 
   // Bind any changes of the input value.
-  $("input.quantity1").bind("click", function() {
+  $("input.quantity1").bind("change click", function() {
     // Concatenate the string '$'' with the multiplication of the vars quantity1 and price1, and append it to the element with the class of .total. The toFixed method formats the number using fixed-point notation, in our case, two decimal places. Using jQuery helper plugin to add commas to numbers every three digits (plugins.js)
-    $(".total1").text("$" + (quantity1 * price1).toFixed(2)).digits();
-
+    $(".total1").text((quantity1 * price1).toFixed(2));
     quantity1 = parseInt($(this).val());
+
     $(".grand-quantity").text(quantity1 + quantity2);
+    var total1 = (quantity1 * price1).toFixed(2);
+
+    /* Sub Total */
+
+    var subtotal = (parseFloat(total1) + total2).toFixed(2);
+    $("#subtotal").text(subtotal);
+
   });
+
   /* Second cart item */
 
   var leatherQuantity = parseInt($("#leather-quantity").text());
@@ -44,28 +52,13 @@ $(document).ready(function() {
   var convertedSuedeCost = Number(suedeCost.replace(/[^0-9\.]+/g, ""));
 
   var total2 = convertedLeatherCost + convertedCanvasCost + convertedSuedeCost;
-  console.log(total2);
-  $(".total2").text("$"+total2).digits();
+
+  $(".total2").text("$" + total2).digits();
 
   /* Badge Counter */
   var grandQuantity = quantity1 + quantity2;
 
   $(".grand-quantity").text(grandQuantity);
 
-  /* Sub Total */
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
